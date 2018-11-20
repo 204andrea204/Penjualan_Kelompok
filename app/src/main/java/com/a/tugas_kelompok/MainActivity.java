976bb92353a8e9dc016d7346b962b1dfd523fb6e
Penjualan <204andrea204@gmail.com>
2018-11-21 01:58:34 +0700

    Awal Commit

---
+++ app/src/main/java/com/a/tugas_kelompok/MainActivity.java
@@ -0,0 +1,25 @@
+package com.a.tugas_kelompok;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView t = (TextView) findViewById(R.id.RegisterHere);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,Register.class);
+                startActivity(i);
+            }
+        });
+    }
+}
